994e575f076839969771ff52ebb64b1674f84072
Emanuel Moraes <emanuelmoraes297@gmail.com>
2023-01-28 15:11:28 -0300

    import nlohmann::json by single_include

--- cmake/libs/nlohmann-json.cmake
+++ cmake/libs/nlohmann-json.cmake
@@ -1,10 +1,16 @@
 # don't run tests
-set(JSON_BuildTests OFF CACHE INTERNAL "")
+# set(JSON_BuildTests OFF CACHE INTERNAL "")
 
-add_subdirectory(lib/nlohmann-json-3.11.2)
+# add_subdirectory(lib/nlohmann-json-3.11.2)
 
-target_link_libraries(
+# target_link_libraries(
+#     GLPlaces
+#     PRIVATE
+#         nlohmann_json::nlohmann_json
+# )
+
+target_include_directories(
     GLPlaces
     PRIVATE
-        nlohmann_json::nlohmann_json
+        lib/nlohmann/json-3.11.2/single_include
 )
